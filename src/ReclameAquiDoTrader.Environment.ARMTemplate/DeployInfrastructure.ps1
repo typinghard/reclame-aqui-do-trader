fc2933607b34c94389098161148c899e09f74893
Elvis Souza <elviscristiansouza@gmail.com>
2021-10-24 09:24:28 -0300

    Update DeployInfrastructure.ps1

--- src/ReclameAquiDoTrader.Environment.ARMTemplate/DeployInfrastructure.ps1
+++ src/ReclameAquiDoTrader.Environment.ARMTemplate/DeployInfrastructure.ps1
@@ -27,8 +27,8 @@ $phpVersion = "OFF"
 $timing = -join($timing, "2. Variables created: ", $stopwatch.Elapsed.TotalSeconds, "`n");
 
 
-if [ $(az group exists --name $serverFarmResourceGroup) = false ]; then
-
+if ($(az group exists --name $serverFarmResourceGroup) -eq 'false') 
+{
 	az group create -l brazilsouth -n $serverFarmResourceGroup
 
 	az deployment group create --resource-group $serverFarmResourceGroup --name $serviceAPIName --template-file "$templatesLocation/WebApp.json" --parameters subscriptionId=$subscriptionId hostingPlanName=$webhostingName location=$location name=$serviceAPIName serverFarmResourceGroup=$serverFarmResourceGroup alwaysOn=$alwaysOn currentStack=$currentStack phpVersion=$phpVersion
@@ -42,4 +42,4 @@ if [ $(az group exists --name $serverFarmResourceGroup) = false ]; then
 												   RavenSettings:DatabaseName=$ravenSettingsDatabaseName `
 												   RavenSettings:Urls:0=$ravenSettingsUrl
 	Write-Host "1. Deployment finished: "$stopwatch.Elapsed.TotalSeconds
-fi
+}
